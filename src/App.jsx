152e76a3943902355379d542bfd0c4925cd033de
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2024-03-09 09:40:45 +0600

    custom-modal-popup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import ModalTest from './custom-modal-popup/modal-test'
+
+function App() {
+  
+
+  return (
+    <>
+      <ModalTest></ModalTest>
+    </>
+  )
+}
+
+export default App
